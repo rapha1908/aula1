75b919f5ef732eff4a1a729b2e4ccc8337bc7351
Raphael Da Silva <rapha.santi@gmail.com>
2025-03-23 15:55:00 +0000

    conexao com o BD

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,24 +1,29 @@
-import "dotenv/config";
+import 'dotenv/config'
 
-import { z } from 'zod';
+import { z } from 'zod'
 
-//define o que cada variavel deve ser (numero,string...)
+// define o que cada variavel deve ser (numero,string...)
 const envScheme = z.object({
-  NODE_ENV: z.enum(['development','production','test']).default('development'),
-  PORT: z.coerce.number().default(3000)
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  PORT: z.coerce.number().default(3000),
+  DATABASE_USER: z.string(),
+  DATABASE_HOST: z.string(),
+  DATABASE_NAME: z.string(),
+  DATABASE_PASSWORD: z.string(),
+  DATABASE_PORT: z.coerce.number(),
 })
 
-//faz a validação propriamente dita
-const _env = envScheme.safeParse(process.env);
+// faz a validação propriamente dita
+const _env = envScheme.safeParse(process.env)
 
-//se der errado
-if(!_env.success) {
-  console.error("Invalid environment variables", _env.error.format())
-  //caso as variaveis de ambiente estiverem erradas nao quero que o servidor suba
-  throw new Error("Invalid environment variables")
+// se der errado
+if (!_env.success) {
+  console.error('Invalid environment variables', _env.error.format())
+  // caso as variaveis de ambiente estiverem erradas nao quero que o servidor suba
+  throw new Error('Invalid environment variables')
 }
 
-//se der certo
-export const env = _env.data;
-
-
+// se der certo
+export const env = _env.data
---
+++ src/lib/pg/db.ts
@@ -0,0 +1,34 @@
+import { Pool, PoolClient } from 'pg'
+import { env } from '@/env'
+
+const CONFIG = {
+  user: env.DATABASE_USER,
+  host: env.DATABASE_HOST,
+  database: env.DATABASE_NAME,
+  password: env.DATABASE_PASSWORD,
+  port: env.DATABASE_PORT,
+}
+
+export class Database {
+  private pool: Pool
+  private client: PoolClient | undefined
+
+  constructor() {
+    this.pool = new Pool(CONFIG)
+    this.connection()
+  }
+
+  private async connection() {
+    try {
+      this.client = await this.pool.connect()
+    } catch (error) {
+      console.error('Error connecting to dataBase:', error)
+    }
+  }
+
+  get clientInstance() {
+    return this.client
+  }
+}
+
+export const database = new Database()
