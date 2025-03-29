fb67cb9ce5f8dc5ea87a2dd9e18554e9764a55a3
Raphael Da Silva <rapha.santi@gmail.com>
2025-03-29 22:49:33 +0000

    findUser

--- src/http/controllers/person/create.ts
+++ src/http/controllers/person/create.ts
@@ -9,17 +9,26 @@ export async function create(request: FastifyRequest, replay: FastifyReply) {
     name: z.string(),
     birth: z.coerce.date(),
     email: z.string().email(),
+    user_id: z.coerce.number(),
   })
 
-  const { cpf, name, birth, email } = registerBodySchema.parse(request.body)
+  const { cpf, name, birth, email, user_id } = registerBodySchema.parse(
+    request.body,
+  )
 
   try {
     const personRepository = new PersonRepository()
     const createPersonUseCase = new CreatePersonUseCase(personRepository)
 
-    await createPersonUseCase.handler({ cpf, name, birth, email })
+    const person = await createPersonUseCase.handler({
+      cpf,
+      name,
+      birth,
+      email,
+      user_id,
+    })
 
-    return replay.status(201).send()
+    return replay.status(201).send(person)
   } catch (error) {
     console.error(error)
     throw new Error('Internal server error')
--- src/http/controllers/user/routes.ts
+++ src/http/controllers/user/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
+import { findUser } from './find'
 
 export async function UserRoutes(app: FastifyInstance) {
   app.post('/user', create)
+  app.get('/user/:id', findUser)
 }
