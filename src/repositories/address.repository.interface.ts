d96e8c2757c46b4142c17e867d97e4fb5aec7743
Raphael Da Silva <rapha.santi@gmail.com>
2025-03-30 19:33:25 +0100

    feat: add address retrieval by person ID in address repository

--- src/repositories/address.repository.interface.ts
+++ src/repositories/address.repository.interface.ts
@@ -1,5 +1,11 @@
 import { Address } from '@/entities/address'
+import { Person } from '@/entities/person.entity'
 
 export interface IAddressRepository {
   create(address: Address): Promise<Address | undefined>
+  findAddressByPersonId(
+    personId: number,
+    page: number,
+    limit: number,
+  ): Promise<(Address & Person)[]>
 }
--- src/repositories/pg/address.repository.ts
+++ src/repositories/pg/address.repository.ts
@@ -1,4 +1,5 @@
 import { Address } from '@/entities/address'
+import { Person } from '@/entities/person.entity'
 import { database } from '@/lib/pg/db'
 
 export class AddressRepository {
@@ -14,5 +15,25 @@ export class AddressRepository {
       ],
     )
     return result?.rows[0]
+  }
+
+  async findAddressByPersonId(
+    personId: number,
+    page: number,
+    limit: number,
+  ): Promise<(Address & Person)[]> {
+    const offset = (page - 1) * limit
+    const query = `
+      SELECT address.*, person.* 
+      FROM address
+      JOIN person ON address.person_id = person.id
+      WHERE person.id = $1
+      LIMIT $2 OFFSET $3
+    `
+    const result = await database.clientInstance?.query<Address & Person>(
+      query,
+      [personId, limit, offset],
+    )
+    return result?.rows || []
   }
 }
