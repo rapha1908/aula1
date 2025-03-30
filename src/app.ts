12ecb2d8bd4abfacdb433421b0c1ae14126f11ef
Raphael Da Silva <rapha.santi@gmail.com>
2025-03-30 11:57:14 +0100

    add custom error handler for validation and internal server errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
 import fastify from 'fastify'
 import { PersonRoutes } from '@/http/controllers/person/routes'
 import { UserRoutes } from '@/http/controllers/user/routes'
+import { ZodError } from 'zod'
+import { env } from 'process'
+import { error } from 'console'
 
 export const app = fastify()
 
 app.register(PersonRoutes)
 app.register(UserRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.format(),
+    })
+  }
+  if (env.NODE_ENV === 'development') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
