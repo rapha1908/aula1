7031a0738498890b7066c25a9d288d1b8f8ac6b5
Raphael Da Silva <rapha.santi@gmail.com>
2025-03-30 11:43:02 +0100

    implement factory functions for creating user and person use cases

---
+++ src/use-cases/factory/make-find-with-person.ts
@@ -0,0 +1,9 @@
+import { UserRepository } from '@/repositories/user.repository'
+import { FindWithPersonUseCase } from '../find-with-person'
+
+export function MakeFindWithPerson() {
+  const userRepository = new UserRepository()
+  const findWithPersonUseCase = new FindWithPersonUseCase(userRepository)
+
+  return findWithPersonUseCase
+}
