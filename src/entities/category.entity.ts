b2b28679abb9f7f5a8c5ee8e638b4f857c9f23a7
Raphael Da Silva <rapha.santi@gmail.com>
2025-04-01 00:32:37 +0100

    feat: refactor address entity and interfaces; add category and product entities with interfaces

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { ICategory } from './models/category.interface'
+
+@Entity({
+  name: 'categories',
+  orderBy: {
+    id: 'ASC',
+  },
+})
+export class Category implements ICategory {
+  @PrimaryGeneratedColumn('increment', {
+    name: 'id',
+    type: 'int',
+  })
+  id?: number | undefined
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+  })
+  name: string
+
+  @Column({
+    name: 'creation_date',
+    type: 'timestamp without time zone',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date
+}
--- src/lib/typeorm/typeorm.ts
+++ src/lib/typeorm/typeorm.ts
@@ -9,7 +9,7 @@ export const appDataSource = new DataSource({
   username: env.DATABASE_USER,
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE_NAME,
-  entities: [],
+  entities: ['src/entities/*.ts'],
   logging: env.NODE_ENV === 'development',
 })
 
